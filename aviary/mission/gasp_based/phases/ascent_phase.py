70722c2f39bf6d00a747a102cbd5c0b64ee7b8b7
MatthewRajan-WA <matthew.rajan@whisperaero.com>
2025-10-09 16:44:49 -0500

    removed alt rate constraint

--- aviary/mission/gasp_based/phases/ascent_phase.py
+++ aviary/mission/gasp_based/phases/ascent_phase.py
@@ -180,12 +180,12 @@ class AscentPhase(PhaseBuilderBase):
             ref=pitch_constraint_ref,
         )
 
-        phase.add_path_constraint(
-            Dynamic.Mission.ALTITUDE_RATE,
-            lower=0,
-            units='ft/s',
-            ref=100.0
-        )
+        # phase.add_path_constraint(
+        #     Dynamic.Mission.ALTITUDE_RATE,
+        #     lower=0,
+        #     units='ft/s',
+        #     ref=100.0
+        # )
 
         phase.add_parameter('t_init_gear', units='s', static_target=True, opt=False, val=38.25)
 
